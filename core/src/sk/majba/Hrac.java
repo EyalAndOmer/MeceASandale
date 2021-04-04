1016b75728c299e5068f9bc98270edea94b8be22
EyalAndOmer <maros.majba2551@gmail.com>
2021-04-04 11:47:16 +0200

    Pridanie mechaniky XP a lvlUp do classy Hrac

--- core/src/sk/majba/Hrac.java
+++ core/src/sk/majba/Hrac.java
@@ -4,14 +4,20 @@ public class Hrac {
     private String meno;
     private int level;
     private Atributy atributy;
+    private int xp;
+    private int xpNeededToLvlUp;
 
-    public Hrac(String meno, int level, Atributy atributy) {
+    public Hrac(String meno, int level, Atributy atributy, int xp) {
         this.meno = meno;
         this.level = level;
         this.atributy = atributy;
+        this.xp = xp;
+        this.xpNeededToLvlUp = 1000;
     }
 
     public void levelUp(Atributy atributy) {
+        this.xp = 0;
+        this.xpNeededToLvlUp *= 2;
         this.atributy = atributy;
     }
 
--- core/src/sk/majba/Main.java
+++ core/src/sk/majba/Main.java
@@ -2,7 +2,8 @@ package sk.majba;
 
 public class Main {
     public static void main(String[] args) {
-        Hrac fero = new Hrac("Fero", 1, new Atributy(5, 5, 1, 1, 1, 1));
+        Hrac fero = new Hrac("Fero", 1,
+                new Atributy(5, 5, 1, 1, 1, 1), 0);
         System.out.println(fero.toString());
         fero.levelUp(new Atributy(5, 5, 5, 1, 1, 1));
         System.out.println(fero.toString());
