3e87a08b2cc98e00e0d7f3be123442d1c815d125
EyalAndOmer <maros.majba2551@gmail.com>
2021-04-14 19:11:44 +0200

    Nova exception NotAllItemsRequiredForItemSetException na kontrolu itemov v ItemSete

---
+++ core/src/sk/majba/items/NotAllItemsRequiredForItemSetException.java
@@ -0,0 +1,7 @@
+package sk.majba.items;
+
+public class NotAllItemsRequiredForItemSetException extends Exception {
+    public NotAllItemsRequiredForItemSetException (String message){
+        super(message);
+    }
+}
